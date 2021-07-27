869fa29c82c9f58bfca2957b6b42edb6accc6c83
kasturivats <40772517+kasturivats@users.noreply.github.com>
2021-07-27 14:47:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function changecolor() {
+    document.getElementById("nameheading").style.color = "green";
+}
+
+function changecolorwhite() {
+    document.getElementById("para").style.color = "white";
+}
+//add more functions//
